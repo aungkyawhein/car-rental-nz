21571026ae248a16cb2d7c6ec5bc17aad42f1438
aungkyawhein <akh.infotech@gmail.com>
2024-04-27 22:10:59 +1200

    Add icons for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,31 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate'
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
+      manifest: {
+        name: 'Car Rental NZ',
+        short_name: 'CarRent',
+        description: 'Car rental price comparison',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
   ],
 })
